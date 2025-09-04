f0ce4bbaf9d95714d769ef14f628b51a8a7cbcee
Divansir-Junior <jscrobut989@gmail.com>
2025-09-03 21:33:31 -0300

    first commit

---
+++ src/main/java/com/neoApps/neoApps/NeoAppsApplication.java
@@ -0,0 +1,13 @@
+package com.neoApps.neoApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoAppsApplication.class, args);
+	}
+
+}
