eb18613cf823435d669d1acdd4231cab32f4e72b
Divansir-Junior <jscrobut989@gmail.com>
2025-09-03 21:47:56 -0300

    Repository ajustado

--- src/main/java/com/neoApps/neoApps/repository/CustomerRepository.java
+++ src/main/java/com/neoApps/neoApps/repository/CustomerRepository.java
@@ -1,4 +1,11 @@
 package com.neoApps.neoApps.repository;
 
-public interface CustomerRepository {
+import com.neoApps.neoApps.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+
 }
