dd49c841a9d38c458a349f12d7d7ddafa5784c2b
Divansir-Junior <jscrobut989@gmail.com>
2025-09-03 22:05:38 -0300

    Classe service populada (dtos)

--- src/main/java/com/neoApps/neoApps/service/CustomerService.java
+++ src/main/java/com/neoApps/neoApps/service/CustomerService.java
@@ -1,4 +1,52 @@
 package com.neoApps.neoApps.service;
 
+import com.neoApps.neoApps.dto.CustomerCreateDTO;
+import com.neoApps.neoApps.dto.CustomerResponseDTO;
+import com.neoApps.neoApps.enums.Gender;
+import com.neoApps.neoApps.enums.States;
+import com.neoApps.neoApps.model.Customer;
+import com.neoApps.neoApps.repository.CustomerRepository;
+import com.neoApps.neoApps.repository.specification.CustomerSpecifications;
+import com.neoApps.neoApps.validation.CustomerValidation;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CustomerService {
+
+    private final CustomerValidation validator;
+    private final CustomerRepository repository;
+
+    public CustomerService(CustomerValidation validator, CustomerRepository repository) {
+        this.validator = validator;
+        this.repository = repository;
+    }
+
+    // Transferencia DTO para segurança
+    private Customer fromCreateDTO(CustomerCreateDTO dto) {
+        Customer c = new Customer();
+        c.setName(dto.name());
+        c.setPassword(dto.password());
+        c.setCpf(dto.cpf());
+        c.setCep(dto.cep());
+        c.setCellPhone(dto.cellPhone());
+        c.setDateOfBirth(dto.dateOfBirth());
+        c.setAge(Period.between(dto.dateOfBirth(), LocalDate.now()).getYears());
+        c.setEmail(dto.email());
+        c.setStreet(dto.street());
+        c.setCity(dto.city());
+        c.setNumberHouse(dto.numberHouse());
+        c.setState(dto.state() != null ? States.valueOf(dto.state()) : null);
+        c.setGender(dto.gender() != null ? Gender.valueOf(dto.gender()) : null);
+        return c;
+    }
+
+    private CustomerResponseDTO toDTO(Customer c) {
+        return new CustomerResponseDTO(c);
+    }
 }
