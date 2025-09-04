6d3cba717253d9f288cf23f7d5b6feb28749dc1b
Divansir-Junior <jscrobut989@gmail.com>
2025-09-03 21:53:52 -0300

    Criado método de validação do CEP

--- src/main/java/com/neoApps/neoApps/validation/CustomerValidation.java
+++ src/main/java/com/neoApps/neoApps/validation/CustomerValidation.java
@@ -1,10 +1,22 @@
 package com.neoApps.neoApps.validation;
 
 
+import com.neoApps.neoApps.model.Customer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerValidation {
+
+    public void validate(Customer customer) {
+        validateCpf(customer.getCpf());
+    }
+
+    // --- Identificação ---
+    private void validateCpf(String cpf) {
+        if (cpf == null || cpf.length() != 11) {
+            throw new IllegalArgumentException("Formato de CPF inválido. Esperado: 11 dígitos.");
+        }
+    }
 
 
 }
